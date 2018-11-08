eb1b73ffc057a999cf0cc96e13c885851f2715e3
alperalanyali <alper@alper-macbook-pro.local>
2018-11-08 19:10:28 +0300

    Initial Commit

---
+++ IChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IChat
+//
+//  Created by Alper on 8.11.2018.
+//  Copyright © 2018 Alper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
